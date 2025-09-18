b6ad1dcac4f2435c802731a6c810e9b631af7401
vintonyshynb <vintonyshynbogdan@gmail.com>
2025-09-18 11:09:24 +0200

    Update index.js

--- web/express/index.js
+++ web/express/index.js
@@ -4,7 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/todos', (req, res) => {
-    res.send('A list of todo items');
+    res.send(`<!doctype html>
+<html>
+    <head>
+        <title>Todos</title>
+    </head>
+    <body>
+        <h1>Bogdan Vintonyshyn</h1>
+    </body>
+</html>`);
 });
 
 app.post('/todos', (req, res) => {
@@ -13,19 +21,19 @@ app.post('/todos', (req, res) => {
 
 app.put('/todos/:id', (req, res) => {
     const { id } = req.params;
-    res.send('Update the todo item with id ${id}');
+    res.send(`Update the todo item with id ${id}`);
 });
 
 app.delete('/todos/:id', (req, res) => {
     const { id } = req.params;
-    res.send('Delete the todo item with id ${id}');
+    res.send(`Delete the todo item with id ${id}`);
 });
 
 app.get('/todos/:id', (req, res) => {
     const { id } = req.params;
-    res.send('Get the todo item with id ${id}');
+    res.send(`Get the todo item with id ${id}`);
 });
 
 app.listen(PORT, () => {
-    console.log('The server is listening on port ${PORT}.')
+    console.log(`The server is listening on port ${PORT}.`);
 });
